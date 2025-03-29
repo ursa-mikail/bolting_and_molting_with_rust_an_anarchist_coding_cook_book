82a73e02a8a9ed5dd6c8a0f5f226aab28017b0d9
mikail-eliyah-00 <eliyah.mikail@gmail.com>
2025-03-28 19:26:21 -0700

    .

---
+++ chapter_01/examples/test-app/src/main.rs
@@ -0,0 +1,8 @@
+mod libs;
+mod utils;
+
+fn main() {
+    println!("Main function called");
+    libs::lib_function();
+    utils::util_function();
+}
---
+++ chapter_01/examples/test-app/src/utils/mod.rs
@@ -0,0 +1,4 @@
+pub fn util_function() {
+    println!("Utility function called");
+}
+
